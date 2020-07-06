0235e207bbb8914c35ce091f2f41eeeddaddf7bc
TSY <example@mail.com>
2020-07-06 11:34:49 +0800

    Add cart, orders, products screens

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const routeName = '/product-detail';
@@ -6,10 +10,53 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("title"),
-      ),
+//      appBar: AppBar(
+//        title: Text(loadedProduct.title),
+//      ),
+      body: CustomScrollView(slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: 300,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text(loadedProduct.title),
+            background: Hero(
+              tag: loadedProduct.id,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            SizedBox(height: 10),
+            Text(
+              '\$${loadedProduct.price}',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                loadedProduct.description,
+                textAlign: TextAlign.center,
+                softWrap: true,
+              ),
+            )
+          ]),
+        ),
+      ]),
     );
   }
 }
